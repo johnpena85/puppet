983696c5f8986611f64bdd65994400ea477a93d1
John Pena <Johnpena85@icloud.com>
2014-08-29 22:59:50 +0000

    puppet apply + new files

---
+++ modules/nginx/manifests/init.pp
@@ -0,0 +1,16 @@
+class nginx {
+package {'nginx':
+ensure => installed,
+}
+service {'nginx':
+ensure => running,
+require => Package['nginx'],
+}
+file {'/var/www/cat-pictures/index.html':
+content => "I can haz a cat picture?\n",
+}
+file {'/etc/nginx/sites-enabled/default':
+source => 'puppet:///modules/nginx/cat-pictures.conf',
+notify => Service['nginx'],
+}
+}
